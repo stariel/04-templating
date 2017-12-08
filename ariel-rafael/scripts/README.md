a9e1e52f90a251041b492a3a85865b9b50a28876
Rafael Malave <rafomalave@gmail.com>
2017-12-07 20:25:33 -0800

    Added readme and arrow functions

---
+++ ariel-rafael/scripts/README.md
@@ -0,0 +1,22 @@
+# Lab 04: HTML Templating w/HandlebarsJS
+
+**Author**: Rafael Malave, Ariel Altaras 
+**Version**: 1.0.0 
+## Overview
+We want our app to render articles with consistent formatting so that I can visit the site often and have the same experience each time.
+
+## Getting Started
+Clone repository to local machine. Run index.html on browser.
+
+## Architecture
+<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+This project's architecure includes SMACSS, MVC. Using Handlebar.js and jQuery.
+
+## Change Log
+Date:   Thu Dec 7 19:06:14 2017 -0800  copied starter code to new ariel-rafael fill
+Date:   Thu Dec 7 19:20:00 2017 -0800 created article template using handlebars.js
+Date:   Thu Dec 7 20:02:29 2017 -0800 did some todos
+
+Noralize.css https://necolas.github.io/normalize.css/
+Handlebar.js http://handlebarsjs.com/
+-->
--- ariel-rafael/scripts/articleView.js
+++ ariel-rafael/scripts/articleView.js
@@ -5,7 +5,8 @@ let articleView = {};
 // TODO: Where possible, refactor methods into arrow functions, including the document.ready() method at the bottom.
 
 // COMMENT: How do arrow functions affect the context of "this"? How did you determine if a function could be refactored?
-// PUT YOUR RESPONSE HERE
+// In an arrow funciton 'this' lexical context is within it's containing block of code. But we want 'this' to be referring to the
+// instance of the article object, so we cannot use arrow funcitons on the selector methods.
 
 articleView.populateFilters = () => {
   $('article').each(function() {
@@ -26,7 +27,7 @@ articleView.populateFilters = () => {
   });
 };
 
-articleView.handleAuthorFilter = function() {
+articleView.handleAuthorFilter = () => {
   $('#author-filter').on('change', function(){
     if ($(this).val()) {
       $('article').hide();
@@ -39,7 +40,7 @@ articleView.handleAuthorFilter = function() {
   });
 };
 
-articleView.handleCategoryFilter = function() {
+articleView.handleCategoryFilter = () => {
   $('#category-filter').on('change', function() {
     if ($(this).val()) {
       $('article').hide();
@@ -52,7 +53,7 @@ articleView.handleCategoryFilter = function() {
   });
 };
 
-articleView.handleMainNav = function() {
+articleView.handleMainNav = () => {
   $('.main-nav').on('click', '.tab', function() {
     $('.tab-content').hide();
     $(`#${$(this).data('content')}`).fadeIn();
@@ -61,7 +62,7 @@ articleView.handleMainNav = function() {
   $('.main-nav .tab:first').click();
 };
 
-articleView.setTeasers = function() {
+articleView.setTeasers = () => {
   $('.article-body *:nth-of-type(n+2)').hide();
   $('article').on('click', 'a.read-on', function(e) {
     e.preventDefault();
@@ -78,7 +79,7 @@ articleView.setTeasers = function() {
   });
 };
 
-$(document).ready(function() {
+$(document).ready(() => {
   articleView.populateFilters();
   articleView.handleCategoryFilter();
   articleView.handleAuthorFilter();
